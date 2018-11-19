04dfc29004b4a61f63fb41ef1fb3be791e3f1041
wangyang <768188667@qq.com>
2018-11-19 22:14:45 +0800

    jest - test reducer, not map action

--- app/containers/Home/__tests__/home.test.js
+++ app/containers/Home/__tests__/home.test.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import TestRenderer from 'react-test-renderer';
-import Home from '../index';
-describe('Home', () => {
-    it('should render TableRow tag with className "rowStyle"', () => {
-      const testRenderer = TestRenderer.create(<Home />);
-      const testInstance = testRenderer.root;
-      expect(testInstance.findByType(TableRow).props.className).toBe(rowStyle);
-    });
+import { getCoinList } from "../action";
+import reducer, { initState } from "../reducer";
+
+test("Get Coin List", () => {
+  const state = initState;
+  const expected = Object.keys({
+    symbol: "ETHSGD",
+    quoteAssetName: "SGD",
+    tradedMoney: 357.0,
+    baseAssetUnit: "",
+    baseAssetName: "ETH",
+    baseAsset: "ETH",
+    tickSize: "0.0000001",
+    prevClose: 140.0,
+    activeBuy: 0.0,
+    high: "140.0000000",
+    lastAggTradeId: -1,
+    low: "140.0000000",
+    matchingUnitType: "STANDARD",
+    close: "140.0000000",
+    quoteAsset: "SGD",
+    productType: null,
+    active: true,
+    minTrade: 0.01,
+    activeSell: 2.55,
+    withdrawFee: "10",
+    volume: "2.5500000",
+    decimalPlaces: 8,
+    quoteAssetUnit: "",
+    open: "140.0000000",
+    status: "TRADING",
+    minQty: 1e-8
+  });
+
+  const result = reducer(state, getCoinList());
+  const rcL = result.coinList.length;
+  if (rcL > 0) {
+    const coinListItemKeys = Object.keys(result.coinList[0]);
+    expect(coinListItemKeys).toEqual(expected);
+  } else {
+    expect(rcL).toEqual(0);
+  }
 });
--- app/containers/Home/reducer.js
+++ app/containers/Home/reducer.js
@@ -1,32 +1,27 @@
-import {
-    COIN_LIST_REQUEST,
-    COIN_LIST_SUCCESS,
-    COIN_LIST_FAIL
-} from "./action";
-  
+import { COIN_LIST_REQUEST, COIN_LIST_SUCCESS, COIN_LIST_FAIL } from "./action";
+
 /*
-* 初始化state
-*/
-  
-  const initState = {
-    coinList: []
-  };
-  /*
-   * reducer
-   */
-  export default function reducer(state = initState, action) {
-    switch (action.type) {
-      case COIN_LIST_REQUEST:
-        return state;
-      case COIN_LIST_SUCCESS:
-        return {
-          ...state,
-          coinList: action.result.data.data
-        };
-      case COIN_LIST_FAIL:
-        return state;
-      default:
-        return state;
-    }
+ * 初始化state
+ */
+
+export const initState = {
+  coinList: []
+};
+/*
+ * reducer
+ */
+export default function reducer(state = initState, action) {
+  switch (action.type) {
+    case COIN_LIST_REQUEST:
+      return state;
+    case COIN_LIST_SUCCESS:
+      return {
+        ...state,
+        coinList: action.result.data.data
+      };
+    case COIN_LIST_FAIL:
+      return state;
+    default:
+      return state;
   }
-  
+}
